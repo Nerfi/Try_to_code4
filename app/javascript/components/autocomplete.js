e1feb3112a8c571069b80ac637fb9f66603a8acb
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-23 15:23:32 +0100

    not working yey  autocomplete

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -3,7 +3,7 @@ function autocomplete() {
     var gymAddress = document.getElementById('gym_address');
 
     if (gymAddress) {
-      var autocomplete = new google.maps.places.Autocomplete(flatAddress, { types: [ 'geocode' ] });
+      var autocomplete = new google.maps.places.Autocomplete(gymAddress, { types: [ 'geocode' ] });
       google.maps.event.addDomListener(gymAddress, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,4 +1,6 @@
 import GMaps from 'gmaps/gmaps.js';
+import { autocomplete } from '../components/autocomplete';
+
 
 
 const mapElement = document.getElementById('map');
@@ -18,3 +20,4 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
 
 
 
+autocomplete();
