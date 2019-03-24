1d6ca16ad05f804fdc5419ec6b2ac4bf3f42bee3
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-24 22:35:35 +0100

    made some changes, especially in boostrap

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,5 +1,7 @@
 class GymsController < ApplicationController
    before_action :set_gym, only: [:show, :edit, :update, :destroy]
+   skip_before_action :authenticate_user!, only: :index
+
 
    def index
     @gyms = Gym.where.not(latitude: nil, longitude: nil)
