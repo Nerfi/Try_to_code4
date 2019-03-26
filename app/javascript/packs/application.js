a24285d50210dba31176f41a0ea51c2811f0e2fe
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-03-26 14:12:48 +0100

    working, next time remeber to add  the path on wich you want to display the localhost:3000/ gyms

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,7 @@
-import "bootstrap";
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+//import "bootstrap";
+//import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 // importing navbar on scroll
-import { initUpdateNavbarOnScroll } from '../components/navbar';
-initUpdateNavbarOnScroll();
+
 
 //import { initMapbox } from '../plugins/init_mapbox';
 
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,7 +1,4 @@
 import GMaps from 'gmaps/gmaps.js';
-//import { autocomplete } from '../components/autocomplete';
-
-
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
@@ -12,12 +9,8 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
     map.setZoom(2);
   } else if (markers.length === 1) {
     map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(10);
+    map.setZoom(14);
   } else {
     map.fitLatLngBounds(markers);
   }
 }
-
-
-
-//autocomplete();
